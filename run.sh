6163ef6552028ba7e9bec576495810a20b81fd28
wangxiangyu <wang.xiangyu@coscoshipping.com>
2023-11-16 21:23:01 +0800

    format pretty a little

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp -r ./models/* /home/centos
-docker run -it \
-    -p 8501:8501 \
-    -v "/home/centos:/models" \
-    tensorflow/serving:2.3.0 \
-    --model_config_file=/models/model.config
